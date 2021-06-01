473e97a3f72033f7f1e6542a2ae38dddc9cf9a7e
gigimic <gigimic@gmail.com>
2021-06-01 15:24:35 +0530

    reviews

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ Sentiment Analysis
 -------------------
 
 RNNs are used to perform sentiment analysis in keras.
+
+We use Recurrent Neural Networks, and in particular LSTMs, to perform sentiment analysis in Keras. Keras has a built-in IMDb movie reviews dataset that we can use, with the same vocabulary size.
+
+label is an integer (0 for negative, 1 for positive), and the review is stored as a sequence of integers.
+To map the integers back to the original words, weu can use imdb.get_word_index(). 
+
+Unlike the Bag-of-Words approach, where we simply summarized the counts of each word in a document, this representation essentially retains the entire sequence of words (minus punctuation, stopwords, etc.). This is critical for RNNs to function. But it also means that now the features can be of different lengths!
+
+In order to feed this data into the RNN, all input documents must have the same length. Let's limit the maximum review length to max_words by truncating longer reviews and padding shorter reviews with a null value (0). 
+This can be done by using the pad_sequences() function in Keras. Here set max_words to 500.
+
+Input is a sequence of words (integer word IDs) of maximum length = max_words, and output is a binary sentiment label (0 or 1).
--- sent_analysis.py
+++ sent_analysis.py
@@ -1,4 +1,6 @@
 from keras.datasets import imdb
+# import tensorflow as tf
+# print('done version tf ', tf.__version__)
 
 # Set the vocabulary size
 vocabulary_size = 5000
@@ -7,4 +9,48 @@ vocabulary_size = 5000
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=vocabulary_size)
 print("Loaded dataset with {} training samples, {} test samples".format(len(X_train), len(X_test)))
 
-print('done')
+# Inspect a sample review and its label
+# label is an integer (0 for negative, 1 for positive), 
+# print("--- Review ---")
+# print(X_train[7])
+print("--- Label ---")
+print(y_train[7])
+
+# These are word IDs that have been preassigned to individual words. 
+# To map them back to the original words, we can use the dictionary returned by imdb.get_word_index().
+
+# Map word IDs back to words
+word2id = imdb.get_word_index()
+id2word = {i: word for word, i in word2id.items()}
+print("--- Review (with words) ---")
+# print([id2word.get(i, " ") for i in X_train[7]])
+print("--- Label ---")
+print(y_train[7])
+
+# Pad sequences: In order to feed this data into the RNN, all input documents must have the same length. 
+# So we can  limit the maximum review length to max_words by truncating longer reviews and 
+# padding shorter reviews with a null value (0). We can do this using the pad_sequences() function in Keras. 
+# Here set max_words to 500.
+
+from keras.preprocessing import sequence
+
+# Set the maximum number of words per document (for both training and testing)
+max_words = 500
+
+# Pad sequences in X_train and X_test
+X_train = sequence.pad_sequences(X_train, maxlen=max_words)
+X_test = sequence.pad_sequences(X_test, maxlen=max_words)
+
+# Design an RNN model for sentiment analysis¶
+
+from keras.models import Sequential
+from keras.layers import Embedding, LSTM, Dense, Dropout
+
+# Design the model
+embedding_size = 32
+model = Sequential()
+model.add(Embedding(vocabulary_size, embedding_size, input_length=max_words))
+model.add(LSTM(100))
+model.add(Dense(1, activation='sigmoid'))
+
+print(model.summary())
